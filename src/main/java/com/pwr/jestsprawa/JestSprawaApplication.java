43d011a4b2ca494c1b6ccaa707d5c8a1bba95b7b
adobek <45313672+adobek@users.noreply.github.com>
2020-10-07 11:08:02 +0200

    Initial commit

---
+++ src/main/java/com/pwr/jestsprawa/JestSprawaApplication.java
@@ -0,0 +1,13 @@
+package com.pwr.jestsprawa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JestSprawaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JestSprawaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pwr/jestsprawa/JestSprawaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pwr.jestsprawa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JestSprawaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
